f68a0eab87e91bdf1288c979df4eae3221e63d43
Nithinsai <42704213+nithinsai263@users.noreply.github.com>
2019-04-10 00:15:33 +0530

    Create pipe.c

---
+++ pipe.c
@@ -0,0 +1,43 @@
+#include"stdio.h"
+#include"stdlib.h"
+#include"unistd.h"
+#include"string.h"
+#include"sys/wait.h"
+void main()
+{
+int fd[2],fd2[2],n;
+char buff[100];
+pid_t p;
+pipe(fd);
+pipe(fd2);
+p=fork();
+if(p>0)
+  {
+  close(fd[0]);
+  printf("Enter any string::--");
+  gets(buff);
+  write(fd[1],buff,strlen(buff));
+  close(fd2[1]);
+  n=read(fd2[0],buff,100);
+  write(1,buff,strlen(buff));
+  }
+else
+  {
+  close(fd[1]);
+  n=read(fd[0],buff,100);
+  for(int i=0;i<strlen(buff);i++)
+    {
+    if(buff[i]>='a' && buff[i]<='z')
+      {
+      buff[i]=buff[i]-32;
+      }
+    else if(buff[i]>='A' && buff[i]<='Z')
+      {
+      buff[i]=buff[i]+32;
+      }
+    }
+  close(fd2[0]);
+  write(fd2[1],buff,strlen(buff));
+  }
+
+}
